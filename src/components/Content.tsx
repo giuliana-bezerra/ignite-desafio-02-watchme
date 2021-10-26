3b78a76730c8128f77b3213ad88a0c867707a262
Giuliana Silva Bezerra <giu.drawer@gmail.com>
2021-10-26 15:18:30 -0300

    improve performance

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,5 @@
+import { AutoSizer, List, ListRowRenderer } from 'react-virtualized';
+
 import { MovieCard } from '../components/MovieCard';
 import { Header } from './Header';
 
@@ -20,11 +22,36 @@ type Movie = {
 };
 
 export function Content({ selectedGenre, movies }: ContentProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style} className='movies-list'>
+        <MovieCard
+          key={movies[index].imdbID}
+          title={movies[index].Title}
+          poster={movies[index].Poster}
+          runtime={movies[index].Runtime}
+          rating={movies[index].Ratings[0].Value}
+        />
+      </div>
+    );
+  };
   return (
     <div className='container'>
       <Header selectedGenre={selectedGenre} />
       <main>
         <div className='movies-list'>
+          {/* <AutoSizer>
+            {({ height, width }) => (
+              <List
+                height={300}
+                width={500}
+                rowHeight={200}
+                overscanColumnCount={5}
+                rowCount={movies.length}
+                rowRenderer={rowRenderer}
+              />
+            )}
+          </AutoSizer> */}
           {movies.map((movie) => (
             <MovieCard
               key={movie.imdbID}
